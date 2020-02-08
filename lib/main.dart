dd95482f28007632b3ecdf739f1af31920e5ddbe
Thomas Braccia <bracciata@gmail.com>
2020-02-08 13:42:48 -0600

    Added code from text to speech.

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,11 @@ import "package:google_maps_webservice/places.dart";
 import 'dart:async';
 import 'dart:io';
 import 'package:google_maps_webservice/directions.dart';
+import 'package:flutter_tts/flutter_tts.dart';
+import 'package:speech_to_text/speech_recognition_result.dart';
+import 'package:speech_to_text/speech_recognition_error.dart';
+import 'package:speech_to_text/speech_to_text.dart';
+enum TtsState { playing, stopped }
 
 final places = GoogleMapsPlaces(
     apiKey: 'AIzaSyC_alUaPxZr-P7wTRhNLYFgM6Yj5XgHQ40');
@@ -35,13 +40,220 @@ class _MyHomePageState extends State<MyHomePage> {
   Position position; // USE THIS FOR CURRENT LOCATION
   Location currentlocation;
   Widget _child;
+  // text to speech
+  FlutterTts flutterTts;
+  dynamic languages;
+  String language;
+  double volume = 0.5;
+  double pitch = 1.0;
+  double rate = 0.4;
+  // speech to text
+  bool _hasSpeech = false;
+  String lastWords = "";
+  String lastError = "";
+  String lastStatus = "";
+  final SpeechToText speech = SpeechToText();
+
+  // text to speech
+  String _newVoiceText =
+      "Hello, Welcome to CrossGuard. Where would you like to go? ";
+
+  TtsState ttsState = TtsState.stopped;
+
+  get isPlaying => ttsState == TtsState.playing;
+
+  get isStopped => ttsState == TtsState.stopped;
+
+  initTts() {
+    flutterTts = FlutterTts();
+
+    _getLanguages();
+
+    flutterTts.setStartHandler(() {
+      setState(() {
+        ttsState = TtsState.playing;
+      });
+    });
+
+    flutterTts.setCompletionHandler(() {
+      setState(() {
+        ttsState = TtsState.stopped;
+        prompt();
+      });
+    });
+
+    flutterTts.setErrorHandler((msg) {
+      setState(() {
+        ttsState = TtsState.stopped;
+      });
+    });
+  }
+
+  Future _getLanguages() async {
+    languages = await flutterTts.getLanguages;
+    if (languages != null) setState(() => languages);
+  }
+
+  Future _speak() async {
+    await flutterTts.setVolume(volume);
+    await flutterTts.setSpeechRate(rate);
+    await flutterTts.setPitch(pitch);
+
+    if (_newVoiceText != null) {
+      if (_newVoiceText.isNotEmpty) {
+        var result = await flutterTts.speak(_newVoiceText);
+        if (result == 1) setState(() => ttsState = TtsState.playing);
+      }
+    }
+  }
+
+  Future stop() async {
+    var result = await flutterTts.stop();
+    if (result == 1) setState(() => ttsState = TtsState.stopped);
+  }
+
+
+  // speech to text
+  Future<void> initSpeechState() async {
+    bool hasSpeech = await speech.initialize(
+        onError: errorListener, onStatus: statusListener);
+
+    if (!mounted) return;
+    setState(() {
+      _hasSpeech = hasSpeech;
+    });
+  }
+  int _expectedResponseTime = 7;
+  Future<void> prompt() async {
+    if (_hasSpeech) {
+      await Future.delayed(const Duration(seconds: 1), () {});
+      startListening();
+      await Future.delayed(Duration(seconds: _expectedResponseTime), () {});
+      stop();
+      checkForLocation();
+    }
+  }
+Future<bool> findLocation(String query) async {
+    String sessionToken = 'xyzabc_1234';
+    PlacesAutocompleteResponse res =
+        await places.autocomplete(query, sessionToken: sessionToken);
+
+    if (res.isOkay) {
+      // list autocomplete prediction
+      for (var p in res.predictions) {
+        print('- ${p.description}');
+      }
+
+      // get detail of the first result
+      PlacesDetailsResponse details = await places.getDetailsByPlaceId(
+          res.predictions.first.placeId,
+          sessionToken: sessionToken);
+
+      print('\nDetails :');
+      print(details.result.formattedAddress);
+      print(details.result.formattedPhoneNumber);
+      print(details.result.url);
+      double latDest = details.result.geometry.location.lat;
+      double lngDest = details.result.geometry.location.lng;
+      Location locationOfDestination = Location(latDest, lngDest);
+      // Ask if location is correct and if so proceed.
+      // TODO add.
+       if(askIfCorrectLocation()){
+      // If yes
+      getDirections(locationOfDestination);
+      // Open camera
+      return true;
+      }else{
+        return false;
+      }
+
+    } else {
+      print(res.errorMessage);
+      return false;
+    }
+
+    places.dispose();
+  }
+  Future<void> getDirections(Location destinationLocation)async{
+   DirectionsResponse res =
+      await directions.directionsWithLocation(currentlocation,destinationLocation,travelMode: TravelMode.walking);
+
+  print(res.status);
+  if (res.isOkay) {
+    print('${res.routes.length} routes');
+    for (var r in res.routes) {
+      print(r.summary);
+      print(r.bounds);
+    }
+  } else {
+    print(res.errorMessage);
+  }
+
+  directions.dispose();
+}
+  Future<void> checkForLocation() async {
+    String parseWords = lastWords.toLowerCase();
+     if (parseWords.contains("stop") || parseWords.contains("no")) {
+      setState(() {
+        _newVoiceText = "Okay, goodbye!";
+      });
+      await Future.delayed(const Duration(seconds: 1), () {});
+      _speak();
+    } else{
+      bool foundLoc = await findLocation(parseWords);
+      if(!foundLoc){
+      // Check for location finds location and sees where they want to go.
+      setState(() {
+        _newVoiceText =
+            "Sorry I did not catch that. Where would you like to go?";
+            _expectedResponseTime = 7;
+      });
+      await Future.delayed(const Duration(seconds: 1), () {});
+      _speak();
+    }
+  }
+
+  void startListening() {
+    lastWords = "";
+    lastError = "";
+    speech.listen(onResult: resultListener);
+    setState(() {});
+  }
+
+  void stopListening() {
+    speech.stop();
+    setState(() {});
+  }
+
+  void cancelListening() {
+    speech.cancel();
+    setState(() {});
+  }
+
+  void resultListener(SpeechRecognitionResult result) {
+    setState(() {
+      lastWords = "${result.recognizedWords} - ${result.finalResult}";
+    });
+  }
+
+  void errorListener(SpeechRecognitionError error) {
+    setState(() {
+      lastError = "${error.errorMsg} - ${error.permanent}";
+    });
+  }
+
+  void statusListener(String status) {
+    setState(() {
+      lastStatus = "$status";
+    });
+  }
   @override
   void initState() {
-    // TODO
     getCurrentLocation();
-    //TODO: Remove this line below.
-    findLocation('allen fieldhouse');
     super.initState();
+    initTts();
+    _speak();
+    initSpeechState();
   }
 
   void getCurrentLocation() async {
@@ -57,7 +269,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Google Maps'),
+        title: Text('CrossGuard'),
       ),
       body: _child,
     );
@@ -87,53 +299,4 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  Future<void> findLocation(String query) async {
-    String sessionToken = 'xyzabc_1234';
-    PlacesAutocompleteResponse res =
-        await places.autocomplete(query, sessionToken: sessionToken);
-
-    if (res.isOkay) {
-      // list autocomplete prediction
-      for (var p in res.predictions) {
-        print('- ${p.description}');
-      }
-
-      // get detail of the first result
-      PlacesDetailsResponse details = await places.getDetailsByPlaceId(
-          res.predictions.first.placeId,
-          sessionToken: sessionToken);
-
-      print('\nDetails :');
-      print(details.result.formattedAddress);
-      print(details.result.formattedPhoneNumber);
-      print(details.result.url);
-      double latDest = details.result.geometry.location.lat;
-      double lngDest = details.result.geometry.location.lng;
-      Location locationOfDestination = Location(latDest, lngDest);
-      // Ask if location is correct and if so proceed.
-      // If yes
-      getDirections(locationOfDestination);
-    } else {
-      print(res.errorMessage);
-    }
-
-    places.dispose();
-  }
-  Future<void> getDirections(Location destinationLocation)async{
-   DirectionsResponse res =
-      await directions.directionsWithLocation(currentlocation,destinationLocation,travelMode: TravelMode.walking);
-
-  print(res.status);
-  if (res.isOkay) {
-    print('${res.routes.length} routes');
-    for (var r in res.routes) {
-      print(r.summary);
-      print(r.bounds);
-    }
-  } else {
-    print(res.errorMessage);
-  }
-
-  directions.dispose();
-}
 }
