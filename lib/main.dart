0c279b0ee27672e26ad0e8697aef8bfa2f070415
Thomas Braccia <bracciata@gmail.com>
2020-02-08 12:56:32 -0600

    Finished implementation of the places and directions apis.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geolocator/geolocator.dart';
+import "package:google_maps_webservice/places.dart";
+import 'dart:async';
+import 'dart:io';
+import 'package:google_maps_webservice/directions.dart';
 
+final places = GoogleMapsPlaces(
+    apiKey: 'AIzaSyC_alUaPxZr-P7wTRhNLYFgM6Yj5XgHQ40');
+final directions =
+    GoogleMapsDirections(apiKey: 'AIzaSyC_alUaPxZr-P7wTRhNLYFgM6Yj5XgHQ40');
 void main() => runApp(App());
- 
+
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -25,40 +33,47 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   GoogleMapController _controller;
   Position position; // USE THIS FOR CURRENT LOCATION
+  Location currentlocation;
   Widget _child;
-  @override 
+  @override
   void initState() {
     // TODO
     getCurrentLocation();
+    //TODO: Remove this line below.
+    findLocation('allen fieldhouse');
     super.initState();
   }
-  void getCurrentLocation() async{
+
+  void getCurrentLocation() async {
     Position res = await Geolocator().getCurrentPosition();
-    setState((){
+    setState(() {
       position = res;
+      currentlocation = Location(res.latitude, res.longitude);
       _child = mapWidget();
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar (
+      appBar: AppBar(
         title: Text('Google Maps'),
       ),
       body: _child,
     );
   }
-  Set<Marker> _createMarker(){
+
+  Set<Marker> _createMarker() {
     return <Marker>[
       Marker(
-        markerId: MarkerId("current_location"),
-        position: LatLng(position.latitude, position.longitude),
-        icon: BitmapDescriptor.defaultMarker,
-        infoWindow: InfoWindow(title: "Current Location")
-      ),
+          markerId: MarkerId("current_location"),
+          position: LatLng(position.latitude, position.longitude),
+          icon: BitmapDescriptor.defaultMarker,
+          infoWindow: InfoWindow(title: "Current Location")),
     ].toSet();
   }
-  Widget mapWidget(){
+
+  Widget mapWidget() {
     return GoogleMap(
       mapType: MapType.normal,
       markers: _createMarker(),
@@ -71,4 +86,54 @@ class _MyHomePageState extends State<MyHomePage> {
       },
     );
   }
+
+  Future<void> findLocation(String query) async {
+    String sessionToken = 'xyzabc_1234';
+    PlacesAutocompleteResponse res =
+        await places.autocomplete(query, sessionToken: sessionToken);
+
+    if (res.isOkay) {
+      // list autocomplete prediction
+      for (var p in res.predictions) {
+        print('- ${p.description}');
+      }
+
+      // get detail of the first result
+      PlacesDetailsResponse details = await places.getDetailsByPlaceId(
+          res.predictions.first.placeId,
+          sessionToken: sessionToken);
+
+      print('\nDetails :');
+      print(details.result.formattedAddress);
+      print(details.result.formattedPhoneNumber);
+      print(details.result.url);
+      double latDest = details.result.geometry.location.lat;
+      double lngDest = details.result.geometry.location.lng;
+      Location locationOfDestination = Location(latDest, lngDest);
+      // Ask if location is correct and if so proceed.
+      // If yes
+      getDirections(locationOfDestination);
+    } else {
+      print(res.errorMessage);
+    }
+
+    places.dispose();
+  }
+  Future<void> getDirections(Location destinationLocation)async{
+   DirectionsResponse res =
+      await directions.directionsWithLocation(currentlocation,destinationLocation,travelMode: TravelMode.walking);
+
+  print(res.status);
+  if (res.isOkay) {
+    print('${res.routes.length} routes');
+    for (var r in res.routes) {
+      print(r.summary);
+      print(r.bounds);
+    }
+  } else {
+    print(res.errorMessage);
+  }
+
+  directions.dispose();
+}
 }
