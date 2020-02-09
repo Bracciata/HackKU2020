643faf2b7fd320601255d1bdffa276b75d49bc54
Thomas Braccia <bracciata@gmail.com>
2020-02-08 18:44:18 -0600

    Finished adding directions and worked on traffic lights. Huge commit.

--- lib/camera.dart
+++ lib/camera.dart
@@ -10,11 +10,15 @@ import 'package:flutter/material.dart';
 import 'detector_painters.dart';
 import 'scanner_utils.dart';
 import 'package:google_maps_webservice/directions.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:flutter_tts/flutter_tts.dart';
+
+enum TtsState { playing, stopped }
 
 class CameraPreviewScanner extends StatefulWidget {
   final DirectionsResponse directions;
 
-  const CameraPreviewScanner({Key key, this.directions}): super(key: key);
+  const CameraPreviewScanner({Key key, this.directions}) : super(key: key);
   @override
   State<StatefulWidget> createState() => _CameraPreviewScannerState();
 }
@@ -25,12 +29,12 @@ class _CameraPreviewScannerState extends State<CameraPreviewScanner> {
   Detector _currentDetector = Detector.cloudLabel;
   bool _isDetecting = false;
   CameraLensDirection _direction = CameraLensDirection.back;
-
   final ImageLabeler _cloudImageLabeler =
       FirebaseVision.instance.cloudImageLabeler();
 
   @override
   void initState() {
+    initTts();
     super.initState();
     _initializeCamera();
   }
@@ -84,10 +88,16 @@ class _CameraPreviewScannerState extends State<CameraPreviewScanner> {
     );
     if (_scanResults is! List<ImageLabel>) return noResultsText;
     painter = LabelDetectorPainter(imageSize, _scanResults);
+    checkForTrafficLights(_scanResults);
 
     return CustomPaint(
       painter: painter,
     );
+  }
+
+  void checkForTrafficLights(var results) {
+    print(results);
+    int x = 7;
   }
 
   Widget _buildImage() {
@@ -130,9 +140,140 @@ class _CameraPreviewScannerState extends State<CameraPreviewScanner> {
     _initializeCamera();
   }
 
+  var stepsList;
+  void getAllStepsAndRelevantInfo() {
+    var waypoints = directions.routes.first.legs.first.steps.iterator;
+    stepsList = [];
+    while (waypoints.moveNext()) {
+      stepsList.add(waypoints.current);
+      print("Adding to steps list.");
+    }
+    print(stepsList);
+  }
+
+  int currentLocationStepIndex = -1;
+  Future<void> checkLocation() async {
+    // Check if close enough and not the same index.
+    await getCurrentLocation();
+    int index = 0;
+    for (var item in stepsList) {
+      if (currentLocationStepIndex != index) {
+        bool withinRange = false;
+        Location locOfStart = item.startLocation;
+        if ((locOfStart.lat - currentLocation.lat).abs() < .0002 &&
+            (locOfStart.lng - currentLocation.lng).abs() < .0002) {
+          withinRange = true;
+        }
+        if (withinRange) {
+          // Say
+          currentLocationStepIndex = index;
+
+          String say;
+          try {
+            if (item.maneuver == null) {
+              say =
+                  'Go ${item.distance.text}. This will take approximately ${item.duration.text}';
+            } else {
+              say =
+                  '${item.maneuver} then go ${item.distance.text}. This will take approximately ${item.duration.text}';
+            }
+          } catch (Exception) {
+            say =
+                'Go ${item.distance.text}. This will take approximately ${item.duration.text}';
+          }
+          currentLocationStepIndex = index;
+          if (index == stepsList.length) {
+            say = "You have arrived!";
+            // Notify them and close the app.
+            // You have arrived
+          }
+          setState(() {
+            _newVoiceText = say;
+          });
+          _speak();
+        }
+      }
+      index += 1;
+    }
+  }
+
+  Location currentLocation;
+  void getCurrentLocation() async {
+    Position res = await Geolocator().getCurrentPosition();
+    setState(() {
+      currentLocation = Location(res.latitude, res.longitude);
+    });
+  }
+
+  // text to speech
+  String _newVoiceText =
+      "Hello, Welcome to CrossGuard. Where would you like to go? ";
+  FlutterTts flutterTts;
+  dynamic languages;
+  String language;
+  double volume = 0.5;
+  double pitch = 1.0;
+  double rate = 0.4;
+  TtsState ttsState = TtsState.stopped;
+  get isPlaying => ttsState == TtsState.playing;
+
+  get isStopped => ttsState == TtsState.stopped;
+
+  initTts() {
+    flutterTts = FlutterTts();
+
+    _getLanguages();
+
+    flutterTts.setStartHandler(() {
+      setState(() {
+        ttsState = TtsState.playing;
+      });
+    });
+
+    flutterTts.setCompletionHandler(() {
+      setState(() {
+        ttsState = TtsState.stopped;
+      });
+    });
+
+    flutterTts.setErrorHandler((msg) {
+      setState(() {
+        ttsState = TtsState.stopped;
+      });
+    });
+  }
+
+  Future _getLanguages() async {
+    languages = await flutterTts.getLanguages;
+    if (languages != null) setState(() => languages);
+  }
+
+  Future _speak() async {
+    await flutterTts.setVolume(volume);
+    await flutterTts.setSpeechRate(rate);
+    await flutterTts.setPitch(pitch);
+
+    if (_newVoiceText != null) {
+      if (_newVoiceText.isNotEmpty) {
+        var result = await flutterTts.speak(_newVoiceText);
+        if (result == 1) setState(() => ttsState = TtsState.playing);
+      }
+    }
+  }
+
+  Future stop() async {
+    var result = await flutterTts.stop();
+    if (result == 1) setState(() => ttsState = TtsState.stopped);
+  }
+
+  var directions;
   @override
   Widget build(BuildContext context) {
+    print(widget.directions);
+    directions = widget.directions;
     _currentDetector = Detector.cloudLabel;
+    getAllStepsAndRelevantInfo();
+    checkLocation();
     return Scaffold(
       appBar: AppBar(
         title: const Text('Cross Guard'),
